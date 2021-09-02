3af03b41b2ca43d2b5f02d13a5a2b60026f15a63
cj <power4j@outlook.com>
2021-09-02 22:34:55 +0800

    feat: 新增InMemorySeqSynchronizer,用于测试

---
+++ sequence-core/src/main/java/com/power4j/kit/seq/core/InMemorySequenceRegistry.java
@@ -0,0 +1,91 @@
+package com.power4j.kit.seq.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Supplier;
+
+/**
+ * @author CJ (power4j@outlook.com)
+ * @date 2021/9/2
+ * @since 1.0
+ */
+public class InMemorySequenceRegistry<T, S extends Sequence<T>> implements SequenceRegistry<T, S> {
+
+	private final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+
+	private final Lock rLock = rwLock.readLock();
+
+	private final Lock wLock = rwLock.writeLock();
+
+	private final Map<String, S> map = new HashMap<>(8);
+
+	@Override
+	public Optional<S> register(String name, S seq) {
+		assert Objects.nonNull(seq);
+		wLock.lock();
+		try {
+			return Optional.ofNullable(map.put(name, seq));
+		}
+		finally {
+			wLock.unlock();
+		}
+	}
+
+	@Override
+	public Optional<S> get(String name) {
+		rLock.lock();
+		try {
+			return Optional.ofNullable(map.get(name));
+		}
+		finally {
+			rLock.unlock();
+		}
+	}
+
+	@Override
+	public Optional<S> remove(String name) {
+		wLock.lock();
+		try {
+			return Optional.ofNullable(map.remove(name));
+		}
+		finally {
+			wLock.unlock();
+		}
+	}
+
+	@Override
+	public S getOrRegister(String name, Supplier<S> supplier) {
+		S seq = get(name).orElse(null);
+		if (Objects.isNull(seq)) {
+			wLock.lock();
+			try {
+				seq = map.get(name);
+				if (Objects.isNull(seq)) {
+					seq = supplier.get();
+					assert Objects.nonNull(seq);
+					map.put(name, seq);
+				}
+			}
+			finally {
+				wLock.unlock();
+			}
+		}
+		return seq;
+	}
+
+	@Override
+	public int size() {
+		rLock.lock();
+		try {
+			return map.size();
+		}
+		finally {
+			rLock.unlock();
+		}
+	}
+
+}
